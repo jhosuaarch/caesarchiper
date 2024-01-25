b29d53b8f8dfa02dc1f03523fe3967f84ce8489b
Jhosua Saut Maruli <80184758+jhosuaarch@users.noreply.github.com>
2024-01-25 22:02:11 +0700

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import string
+
+
+banner = """ ___ ___ ___ ___ ___ ___ 
+|  _| .'| -_|_ -| .'|  _|
+|___|__,|___|___|__,|_|  
+\tmade by @jhosua
+"""
+shift = 3
+plaintext = ""
+chipertext = ""
+alfabet = string.ascii_lowercase
+
+
+def caesarchiper(text, mode):
+     r = ""
+     for i in text:
+          try:
+               if i == " ":
+                    r += " "
+               else:
+                    if mode == "dec":
+                         next = (alfabet.index(i.lower()) - shift) % 26
+                    if mode == "enc":
+                         next = (alfabet.index(i.lower()) + shift) % 26
+                    r += alfabet[next]
+          except ValueError:
+               print("(skip) " + i + " non alfabet")
+               continue
+               
+     return r.upper()
+
+
+while True:
+     os.system("clear")
+     print(banner)
+     select = input("1. encryption\n2. decryption\n0. exit\n\n>> ")
+     
+     # mode
+     mode = "enc" if select == "1" else "dec" if select == "2" else sys.exit()
+     text = input("\nplaintext  : ") if mode == "enc" else input("\nchipertext : ") 
+     res = caesarchiper(text, mode)
+     
+     # show result
+     print("chipertext : " + res if mode == "enc" else "plaintext  : " + res)
+
+     input("\n\npress enter to continue... ")
